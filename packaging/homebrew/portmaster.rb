2d34c36c261323a3a560baf8f7f52c08cf5b22cf
RichardFlp <richardabrahamyan9@gmail.com>
2026-08-10 00:41:08 +0400

    Update packaging manifests with v1.0.0 SHA256 hashes

--- packaging/homebrew/portmaster.rb
+++ packaging/homebrew/portmaster.rb
@@ -2,7 +2,7 @@ class Portmaster < Formula
   desc "Fast, cross-platform port and process management for developers"
   homepage "https://github.com/RichardFlp/portmaster"
   url "https://github.com/RichardFlp/portmaster/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "REPLACE_WITH_SOURCE_TARBALL_SHA256"
+  sha256 "503b4fd86c8c4b220644d57018ee60cfcc1534a5701f5947a6d2335fd293253a"
   license "MIT"
 
   depends_on "go" => :build
